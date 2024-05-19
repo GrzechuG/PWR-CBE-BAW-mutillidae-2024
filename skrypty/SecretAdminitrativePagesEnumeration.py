767563411ef9c22bfcc6ec5a06c05eaf8f62cef5
Patryk Duniak <93217316+PatrykDuniak@users.noreply.github.com>
2024-05-20 00:03:01 +0200

    Add files via upload

--- skrypty/SecretAdminitrativePagesEnumeration.py
+++ skrypty/SecretAdminitrativePagesEnumeration.py
@@ -3,17 +3,20 @@ from bs4 import BeautifulSoup
 from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor
 
-def find_secret_pages(url):
+# A5 - Directory Browsing, file: lista_url_directory.txt, check_value = 'Validation Error: 404 - Page Not Found'
+# A7 - Secret Administrative Pages, file: lista_url_php.txt, check_value = '404 Not Found'
+
+def find_secret_pages(url, check_value):
     response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
-        if 'Validation Error: 404 - Page Not Found' in soup.get_text():
+        if check_value in soup.get_text():
             return None
         else:
             title = soup.title.text if soup.title else "Null"
             return (url, title)
 
-def pages_enum(popular_url, main_url, cores=8):
+def pages_enum(popular_url, main_url, check_value, cores=8):
     urls_to_check = []
     with open(popular_url, 'r') as f:
         for line in f:
@@ -21,13 +24,14 @@ def pages_enum(popular_url, main_url, cores=8):
             urls_to_check.append(url)
 
     with ThreadPoolExecutor(max_workers=cores) as executor:
-        results = list(tqdm(executor.map(find_secret_pages, urls_to_check), total=len(urls_to_check), desc="Przetwarzanie podstron"))
+        results = list(tqdm(executor.map(lambda url: find_secret_pages(url, check_value), urls_to_check), total=len(urls_to_check), desc="Przetwarzanie podstron"))
 
     for result in results:
         if result is not None:
             print(f"-Page Name '{result[0]}': {result[1]}")
 
 if __name__ == "__main__":
-    popular_url = "lista_url.txt" 
-    url = "http://192.168.64.141/mutillidae/index.php?page="
-    pages_enum(popular_url, url)
+    popular_url = "lista_url_directory.txt" 
+    check_value = '404 Not Found'
+    url = "http://192.168.64.141/mutillidae/"
+    pages_enum(popular_url, url, check_value)
