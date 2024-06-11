4ec3113a249b3c3f54d22b15e7cd7a4256202590
Patryk Duniak <93217316+PatrykDuniak@users.noreply.github.com>
2024-06-11 20:41:03 +0200

    Update Tester.py

--- skrypty/Tester/Tester.py
+++ skrypty/Tester/Tester.py
@@ -1,15 +1,14 @@
 import argparse
-from Exploitation import http_pollution, sqlinjection, command_inj, xmleei
+from Exploitation import http_pollution, sqlinjection, command_inj, xmleei, htmli, frame_inj
 
 
 def main():
     parser = argparse.ArgumentParser(description="Porównuje dwie strony HTML.")
-    parser.add_argument("--url", type=str, help="Adres URL strony wraz z parametrem")
+    parser.add_argument("--url", type=str, help="Adres URL strony (wraz z parametrem jeżeli http_polution)")
+    parser.add_argument("--http-polution", action="store_true", help="HTTP polution")
     parser.add_argument(
         "--parameter", nargs="*", type=str, help='Podaj parametry GET po spacjach w ""'
     )
-    parser.add_argument("--autoforms", action="store_true", help="Scrap all forms")
-    parser.add_argument("--http-polution", action="store_true", help="HTTP polution")
     # parser.add_argument('--xssget', action='store_true', help='XSS via GET')
     # parser.add_argument('--xsspost', action='store_true', help='XSS via POST')
     parser.add_argument(
@@ -29,6 +28,14 @@ def main():
     parser.add_argument(
         "--xmleei", action="store_true", help="XML External Entity Injection"
     )
+    parser.add_argument(
+        "--htmli",
+        type=str,
+        nargs="?",
+        const="Onsite",
+        help='HTML Injection currently supported: "Onsite", "Header", "Cookie"',
+    )
+    parser.add_argument("--autoforms", action="store_true", help="Scrap all forms")
     parser.add_argument("--postdata", nargs="*", help="Post Data")
     args = parser.parse_args()
 
@@ -44,6 +51,8 @@ def main():
         print(command_inj(args.url, args.comminj, args.autoforms, args.postdata))
     elif args.xmleei:
         print(xmleei(args.url, args.autoforms, args.postdata))
+    elif args.htmli:
+        print(htmli(args.url, args.htmli, args.autoforms))
     else:
         print("Nie podano odpowiedniego argumentu.")
 
